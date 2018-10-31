d40e63952c16c2815f3120f300953029b0b357d2
Brandon Williams <brandonwilliams9@gmail.com>
2018-10-31 15:46:59 +0100

    Use group percentage as column height

--- explore.R
+++ explore.R
@@ -4,6 +4,8 @@ library(cowplot)
 
 source("dfp_ggplot_theme.R")
 
+extrafont::font_import()
+
 colors <- c(
   "blue" = "#124073",
   "dark_blue" = "#0A2645",
@@ -36,8 +38,8 @@ educ_lvls <- c(
 
 race_lvls <- c(
   "White",
-  "Black",
   "Hispanic",
+  "Black",
   "Asian",
   "Other",
   "[DO NOT READ] Refused",
@@ -45,17 +47,34 @@ race_lvls <- c(
 )
 
 df <- dat %>%
-  filter(partyid == "Independent (No party)") %>%  # should "or as a member of another political party" be included? 
+  filter(
+    partyid == "Independent (No party)",  # should "or as a member of another political party" be included? 
+    likely == "Already voted"
+  ) %>% 
   select(response, Age = ager, Education = educ, Gender = gender, `Race / Ethnicity` = race_eth, State = state) %>%
   mutate(
     Education = factor(Education, levels = educ_lvls),
     `Race / Ethnicity` = factor(`Race / Ethnicity`, levels = race_lvls)
   )
 
-plot_demo <- function(col) {
+addline_format <- function(x, ...) {
+  gsub('\\s', '\n', x)
+}
+
+nudge_y <- c(
+  "Age" = 0.02,
+  "Education" = 0.018,
+  "Gender" = 0.035,
+  "Race / Ethnicity" = 0.04
+)
+
+prepare_data <- function(col) {
   df$col <- df[[col]]
-  dfp <- df %>%
-    filter(!(col %in% c("[DO NOT READ] Don't know/Refused", "[DO NOT READ] Refused"))) %>%
+  
+  dfc <- df %>%
+    filter(!(col %in% c("[DO NOT READ] Don't know/Refused", "[DO NOT READ] Refused")))
+  
+  df1 <- dfc %>%
     group_by(col, response) %>%
     tally() %>%
     filter(response %in% c("Dem", "Rep")) %>%
@@ -63,16 +82,39 @@ plot_demo <- function(col) {
     select(-n) %>%
     spread(response, p) %>%
     mutate(diff = Dem - Rep)
+  
+  df2 <- dfc %>%
+    group_by(col) %>%
+    tally() %>%
+    mutate(p = n / sum(n))
+  
+  dfp <- inner_join(df1, df2)
+  
   df$col <- NULL
   
-  dfp %>%
-    ggplot(aes(fct_rev(col), diff, fill = diff)) +
+  dfp
+}
+
+
+plot_demo <- function(col) {
+  dfp <- prepare_data(col)
+  
+  g <- dfp %>%
+    ggplot(aes(fct_rev(col), p, fill = diff)) +
     geom_col(width = 0.8) + 
+    geom_text(aes(label = scales::percent(p)), nudge_y = nudge_y[[col]], family='Montserrat-Regular') + 
     coord_flip() +
-    scale_fill_gradient2(low = colors['red'], high = colors['blue']) + 
-    labs(title = paste("Early voting from Independents by", col), x = "", y = "", fill = "") + 
+    scale_fill_gradient2(low = colors['red'], high = colors['blue'], mid = colors['light_gray']) + 
+    labs(title = "", x = "", y = "", fill = "") + 
     theme_dfp() +
-    theme(legend.key.width = unit(2.5, "cm"))
+    theme(legend.key.width = unit(1.5, "cm"))
+  
+  if (col == "Education") {
+    g <- g + 
+      scale_x_discrete(labels = addline_format(rev(levels(df$Education))[3:length(levels(df$Education))]))
+  }
+  
+  g
 }
 
 plots <- c()
@@ -84,6 +126,9 @@ demos <- df %>%
 for (col in demos) {
   plots[[col]] <- plot_demo(col)
 }
+
+g <- plot_grid(plotlist = plots)
+g
 
 plots[['Age']]
 plots[['Education']]
@@ -98,25 +143,18 @@ save_plot("plots/race_eth.png", plots[['Race / Ethnicity']])
 
 ## States
 
-dfs <- df %>%
-  filter(!(State %in% c("[DO NOT READ] Don't know/Refused", "[DO NOT READ] Refused"))) %>%
-  group_by(State, response) %>%
-  tally() %>%
-  filter(response %in% c("Dem", "Rep")) %>%
-  mutate(p = n / sum(n)) %>%
-  select(-n) %>%
-  spread(response, p) %>%
-  mutate(diff = Dem - Rep) %>%
+dfs <- prepare_data('State') %>%
+  rename(State = col) %>%
   mutate(region = plyr::mapvalues(toupper(State), state.abb, tolower(state.name), warn_missing = FALSE))
 
 states <- map_data("state") %>%
   left_join(dfs)
 
-plots[['State']] <- ggplot(states, aes(x = long, y = lat, group = group, fill = diff), color = 'white') + 
+plots[['State']] <- ggplot(states, aes(x = long, y = lat, group = group, fill = n), color = 'white') + 
   geom_polygon() + 
   coord_fixed(1.3) + 
-  scale_fill_gradient2(low = colors['red'], high = colors['blue'], na.value = "white") + 
-  labs(title = "Early voting by state", x = "", y = "", fill = "") + 
+  scale_fill_gradient(na.value = "white", low = colors['light_gray'], high = colors['dark_blue']) + 
+  labs(title = "", x = "", y = "", fill = "") + 
   theme_dfp() + 
   theme(axis.ticks = element_blank(), axis.text = element_blank(), legend.key.width = unit(3, "cm"))
 
