b41fbc34faf437a29b9d237fb1c4b28d80a503ab
Brandon Williams <brandonwilliams9@gmail.com>
2018-11-04 23:04:42 +0100

    Overall policy support

--- src/policy_support_self_id.R
+++ src/policy_support_self_id.R
@@ -40,8 +40,71 @@ dfc <- dat %>%
   mutate(partyid = factor(partyid, levels = c("Other", "Republican", "Democrat"))) %>%
   mutate(likely = factor(likely, levels = likely_lvls))
 
-table(dfc$likely)
-table(dfc$partyid)
+## Overall policy support
+
+for (i in 1:length(questions)) {
+  
+  if (i %in% c(1, 2)) {
+    support_colors <- c(
+      "Support" = colors[['blue']],
+      "Oppose" = colors[['red']],
+      "Don't Know" = colors[['light_gray']]
+    )
+  } else if (i %in% c(3, 6)) {
+    support_colors <- c(
+      "Support" = colors[['red']],
+      "Oppose" = colors[['blue']],
+      "Don't Know" = colors[['light_gray']]
+    )
+  } else {
+    support_colors <- c(
+      "Agree" = colors[['red']],
+      "Disagree" = colors[['blue']],
+      "Don't Know" = colors[['light_gray']]
+    )
+  }
+  
+  question <- questions[[i]]
+  col <- names(questions[i])
+  
+  df <- dfc
+  df$col <- df[[col]]
+  df <- df %>%
+    drop_na(col) %>%
+    # filter(col != "Don't know") %>%
+    group_by(col) %>%
+    # tally() %>%
+    summarize(n = n(), w = sum(w_LV)) %>%
+    mutate(p = w / sum(w)) %>%
+    mutate(question = question) %>%
+    mutate(col = str_to_title(col)) %>%
+    mutate(col = fct_rev(factor(col, levels = c("Support", "Oppose", "Agree", "Disagree", "Don't Know"))))
+  
+  g <- ggplot(df, aes(col, p, fill = col)) + 
+    geom_col() + 
+    coord_flip() +
+    geom_text(aes(label = scales::percent(p)), nudge_y = 0.06, family = 'Montserrat-Regular', size = 2.8) +
+    scale_fill_manual(values = support_colors) + 
+    scale_y_continuous(labels = function(x) scales::percent(x, accuracy = 1), limits = c(0, 1)) +
+    labs(title = str_wrap(question, width = 44), subtitle = paste("n =", sum(df$n)), x = "", y = "") +
+    guides(fill = FALSE) + 
+    theme_dfp() + 
+    theme(
+      plot.title = element_text(hjust = 0, size = 10, margin = margin(b = 10), face = "bold", family = 'FuturaBT-Heavy'),
+      plot.subtitle = element_text(hjust = 0, size = 8, family = 'Montserrat-Regular')
+    )
+  
+  dfs[[question]] <- df
+  plots[[question]] <- g
+}
+
+
+g <- plot_grid(plotlist = plots)
+g
+save_plot("plots/policy_support_overall.png", g, base_aspect_ratio = 3.4)
+
+
+## Policy support by party
 
 for (i in 1:length(questions)) {
   question <- questions[[i]]
@@ -53,17 +116,25 @@ for (i in 1:length(questions)) {
     drop_na(col) %>%
     filter(col != "Don't know") %>%
     group_by(partyid, col) %>%
-    tally() %>%
-    mutate(p = n / sum(n)) %>%
-    filter(col %in% c("Support", "support", "agree"))
+    # tally() %>%
+    summarize(n = n(), w = sum(w_LV)) %>%
+    mutate(p = w / sum(w)) 
 
-  if ('agree' %in% df$col) {
-    subtitle <- paste("% of party that agrees; n =", sum(df$n))
+  # if ('agree' %in% df$col) {
+  #   subtitle <- paste("% of party that agrees; n =", sum(df$n))
+  # } else {
+  #   subtitle <- paste("% of party that supports measure; n =", sum(df$n))
+  # }
+  
+  if (i %in% c(4, 5)) {
+    subtitle <- "% of party in agreement"
   } else {
-    subtitle <- paste("% of party that supports measure; n =", sum(df$n))
+    subtitle <- "% of party that supports measure"
   }
 
-  g <- ggplot(df, aes(partyid, p)) +
+  g <- df %>%
+    filter(col %in% c("Support", "support", "agree")) %>%
+    ggplot(aes(partyid, p)) +
     geom_col(width = 0.75) +
     coord_flip() +
     geom_text(aes(label = scales::percent(p)), nudge_y = 0.06, family = 'Montserrat-Regular', size = 2.8) +
@@ -79,33 +150,12 @@ for (i in 1:length(questions)) {
   plots[[question]] <- g
 }
 
-df <- dfc
-df$col <- df[[col]]
-
-df <- df %>%
-  drop_na(col) %>%
-  filter(col != "Don't know") %>%
-  group_by(partyid, likely, col) %>%
-  tally() %>%
-  mutate(p = n / sum(n)) %>%
-  filter(col %in% c("Support", "support", "agree"))
-
-ggplot(df, aes(partyid, p)) +
-  geom_col(width = 0.75) +
-  coord_flip() +
-  geom_text(aes(label = scales::percent(p)), nudge_y = 0.06, family = 'Montserrat-Regular', size = 2.8) +
-  facet_wrap(~likely) +   
-  scale_y_continuous(labels = function(x) scales::percent(x, accuracy = 1), limits = c(0, 1)) +
-  labs(title = str_wrap(question, width = 44), subtitle = subtitle, x = "", y = "") +
-  theme_dfp() +
-  theme(
-    plot.title = element_text(hjust = 0, size = 10, margin = margin(b = 10), face = "bold", family = 'FuturaBT-Heavy'),
-    plot.subtitle = element_text(hjust = 0, size = 8, family = 'Montserrat-Regular')
-  )
-
 g <- plot_grid(plotlist = plots)
+g
 save_plot("plots/policy_support_self_id.png", g, base_aspect_ratio = 3.4)
 
+
+## Write data
 
 df <- data.frame()
 for (i in 1:length(dfs)) {
